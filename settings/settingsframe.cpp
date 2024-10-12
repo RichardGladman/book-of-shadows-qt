94f059ae0c5ad8f7f13eeb6100fb49feb61fa190
Richard Gladman <richard@thefifthcontinent.com>
2024-10-12 10:49:21 +0100

    Fix bug when saving settings

--- settings/settingsframe.cpp
+++ settings/settingsframe.cpp
@@ -2,12 +2,20 @@
 #include "ui_settingsframe.h"
 
 #include <QFileDialog>
+#include <QMessageBox>
+#include <QSettings>
 
 SettingsFrame::SettingsFrame(QWidget *parent)
     : QFrame(parent)
     , ui(new Ui::SettingsFrame)
 {
     ui->setupUi(this);
+
+    QSettings settings("TheFifthContinent", "BookOfShadows");
+
+    settings.beginGroup("Paths");
+    ui->directoryLineEdit->setText(settings.value("datadir").toString());
+    settings.endGroup();
 }
 
 SettingsFrame::~SettingsFrame()
@@ -18,7 +26,7 @@ SettingsFrame::~SettingsFrame()
 void SettingsFrame::on_directoryButton_clicked()
 {
     QString dirName = QFileDialog::getExistingDirectory(this, "Choose data directory", QString(), QFileDialog::ShowDirsOnly);
-    if (dirName != "") {
+    if (!dirName.isEmpty()) {
         ui->directoryLineEdit->setText(dirName);
     }
 }
@@ -26,6 +34,16 @@ void SettingsFrame::on_directoryButton_clicked()
 
 void SettingsFrame::on_saveButton_clicked()
 {
+    QString dirName = ui->directoryLineEdit->text();
 
+    if (dirName.isEmpty()) {
+        QMessageBox::warning(this, "Validation Error", "You must choose a directory");
+        return;
+    }
+
+    QSettings settings("TheFifthContinent", "BookOfShadows");
+
+    settings.beginGroup("Paths");
+    settings.setValue("datadir", dirName);
+    settings.endGroup();
 }
-
