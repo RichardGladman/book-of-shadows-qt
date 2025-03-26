1e10e6bffacca11f00f03831939ef3959c9c9491
Richard Gladman <richard@thefifthcontinent.com>
2025-03-26 13:07:20 +0000

    Add tree model

---
+++ trees/treemodel.cpp
@@ -0,0 +1,94 @@
+#include "treemodel.h"
+
+QSqlQuery TreeModel::list()
+{
+    QSqlQuery query;
+
+    query.prepare("SELECT id, name FROM trees ORDER BY name");
+    query.exec();
+
+    return query;
+}
+
+TreeModel TreeModel::load(int id)
+{
+    TreeModel model(0, "", "");
+
+    QSqlQuery query;
+    query.prepare("SELECT * FROM trees WHERE id=?");
+    query.addBindValue(id);
+
+    if (query.exec() && query.next()) {
+        model.id(query.value(0).toInt());
+        model.name(query.value(1).toString());
+        model.description(query.value(2).toString());
+    }
+
+    return model;
+}
+
+TreeModel::TreeModel(int id, QString name, QString description) :
+    m_id {id}, m_name {name}, m_description {description} {}
+TreeModel::TreeModel(QString name, QString description) : TreeModel(0, name, description) {}
+TreeModel::TreeModel(QString name) : TreeModel(0, name, "");
+
+bool TreeModel::save()
+{
+    QString sql;
+
+    if (m_id == 0) {
+        sql = "INSERT INTO trees(name, description) VALUES(?, ?)";
+    } else {
+        sql = "UPDATE trees SET name=?, description=? WHERE id=?";
+    }
+
+    QSqlQuery query;
+    query.prepare(sql);
+
+    query.addBindValue(m_name);
+    query.addBindValue(m_description);
+
+    if (m_id != 0) {
+        query.addBindValue(m_id);
+    }
+
+    return query.exec();
+}
+
+void TreeModel::remove()
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM trees WHERE id=>");
+    query.addBindValue(m_id);
+    query.exec();
+}
+
+int TreeModel::id() const
+{
+    return m_id;
+}
+
+QString TreeModel::name() const
+{
+    return m_name;
+}
+
+QString TreeModel::description() const
+{
+    return m_description;
+}
+
+void TreeModel::id(int id)
+{
+    m_id = id;
+}
+
+void TreeModel::name(QString name)
+{
+    m_name = name;
+}
+
+void TreeModel::description(QString description)
+{
+    m_description = description;
+}
---
+++ trees/treemodel.h
@@ -0,0 +1,34 @@
+#ifndef TREEMODEL_H
+#define TREEMODEL_H
+
+#include <QSqlQuery>
+#include <QString>
+
+class TreeModel
+{
+public:
+    static QSqlQuery list();
+    static TreeModel load(int id);
+
+    TreeModel(int id, QString name, QString description);
+    TreeModel(QString name, QString description);
+    TreeModel(QString name);
+
+    int id() const;
+    QString name() const;
+    QString description() const;
+
+    void id(int id);
+    void name(QString name);
+    void description(QString description);
+
+    bool save();
+    void remove();
+
+private:
+    int m_id;
+    QString m_name;
+    QString m_description;
+};
+
+#endif // TREEMODEL_H
