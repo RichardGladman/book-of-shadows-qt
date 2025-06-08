c182c1c28e44a7d4e1a3a47d937c61ea2697c947
Richard Gladman <richard@thefifthcontinent.com>
2025-06-08 10:19:55 +0100

    Populate combo boxes for one to one relations

--- runestones/runestoneform.cpp
+++ runestones/runestoneform.cpp
@@ -1,11 +1,32 @@
 #include "runestoneform.h"
 #include "ui_runestoneform.h"
 
+#include <QSqlQuery>
+
+#include "../polarity/polaritymodel.h"
+#include "../planets/planetmodel.h"
+#include "../zodiac/zodiacmodel.h"
+
 RunestoneForm::RunestoneForm(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::RunestoneForm)
 {
     ui->setupUi(this);
+
+    QSqlQuery query = PolarityModel::list();
+    while (query.next()) {
+        ui->polarityCombo->addItem(query.value(1).toString(), query.value(0).toInt());
+    }
+
+    query = PlanetModel::list();
+    while (query.next()) {
+        ui->planetCombo->addItem(query.value(1).toString(), query.value(0).toInt());
+    }
+
+    query = ZodiacModel::list();
+    while (query.next()) {
+        ui->zodiacCombo->addItem(query.value(1).toString(), query.value(0).toInt());
+    }
 }
 
 RunestoneForm::~RunestoneForm()
