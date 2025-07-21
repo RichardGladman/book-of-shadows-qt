928822395dc8e61f808e4332b8ea40a736358221
Richard Gladman <richard@thefifthcontinent.com>
2025-07-21 12:41:35 +0100

    Implement rune slots

--- spellbook/runespellform.cpp
+++ spellbook/runespellform.cpp
@@ -28,3 +28,164 @@ void RuneSpellForm::on_fehuButton_clicked()
     scene->addItem(pixmap);
 }
 
+
+void RuneSpellForm::on_uruzButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/uruz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_thurisazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/thurisaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_ansuzButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/ansuz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_raidoButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/raido.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_kaunazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/kaunaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_geboButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/gebo.pmg"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_wunjoButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/wunjo.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_hagalazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/hagalaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_nauthizButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/nauthiz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_isaButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/isa.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_jeraButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/jera.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_eihwazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/wihwaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_perthoButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/pertho.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_algizButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/algiz.pmg"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_sowuloButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/sowulo.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_teiwazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/teiwaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_berkanaButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/berkana.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_ehwazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/ehwaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_mannazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/mannaz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_laguzButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/laguz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_inguzButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/inguz.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_othilaButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/othila.png"));
+    scene->addItem(pixmap);
+}
+
+
+void RuneSpellForm::on_dagazButton_clicked()
+{
+    ResizablePixmap *pixmap = new ResizablePixmap(QPixmap(":/glyphs/dagaz.png"));
+    scene->addItem(pixmap);
+}
+
--- spellbook/runespellform.h
+++ spellbook/runespellform.h
@@ -19,6 +19,52 @@ public:
 private slots:
     void on_fehuButton_clicked();
 
+    void on_uruzButton_clicked();
+
+    void on_thurisazButton_clicked();
+
+    void on_ansuzButton_clicked();
+
+    void on_raidoButton_clicked();
+
+    void on_kaunazButton_clicked();
+
+    void on_geboButton_clicked();
+
+    void on_wunjoButton_clicked();
+
+    void on_hagalazButton_clicked();
+
+    void on_nauthizButton_clicked();
+
+    void on_isaButton_clicked();
+
+    void on_jeraButton_clicked();
+
+    void on_eihwazButton_clicked();
+
+    void on_perthoButton_clicked();
+
+    void on_algizButton_clicked();
+
+    void on_sowuloButton_clicked();
+
+    void on_teiwazButton_clicked();
+
+    void on_berkanaButton_clicked();
+
+    void on_ehwazButton_clicked();
+
+    void on_mannazButton_clicked();
+
+    void on_laguzButton_clicked();
+
+    void on_inguzButton_clicked();
+
+    void on_othilaButton_clicked();
+
+    void on_dagazButton_clicked();
+
 private:
     Ui::RuneSpellForm *ui;
     QGraphicsScene *scene;
