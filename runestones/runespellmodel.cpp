5e235bb88cf3dc591d6b4dbdfc48de01cf90683e
Richard Gladman <richard@thefifthcontinent.com>
2025-09-04 13:49:39 +0100

    Implement load rune spell

--- runestones/runespellmodel.cpp
+++ runestones/runespellmodel.cpp
@@ -4,7 +4,7 @@ QSqlQuery RunespellModel::list()
 {
     QSqlQuery query;
 
-    query.prepare("SWLWCT id title FROM runespalls");
+    query.prepare("SELECT id title FROM runespells");
     query.exec();
 
     return query;
@@ -12,7 +12,19 @@ QSqlQuery RunespellModel::list()
 
 RunespellModel RunespellModel::load(int id)
 {
-    return RunespellModel();
+    RunespellModel model {};
+
+    QSqlQuery query;
+    query.prepare("SELECT * FROM runespells WHERE id=?");
+    query.addBindValue(id);
+
+    if (query.exec() && query.next()) {
+        model.id(query.value(0).toInt());
+        model.title(query.value(1).toString());
+        model.description(query.value(2).toString());
+    }
+
+    return model;
 }
 
 RunespellModel::RunespellModel() {}
