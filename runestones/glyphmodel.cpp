3b6f7f1a97254f0d9eafd1dba167f2fee3221dee
Richard Gladman <richard@thefifthcontinent.com>
2025-09-05 11:44:20 +0100

    Implement basic Glyph model

---
+++ runestones/glyphmodel.cpp
@@ -0,0 +1,95 @@
+#include "glyphmodel.h"
+
+QSqlQuery GlyphModel::list(int runespell)
+{
+    return QSqlQuery {};
+}
+
+GlyphModel GlyphModel::load(int id)
+{
+    return GlyphModel {};
+}
+
+GlyphModel::GlyphModel(int id, QString name, int runespell, int xpos, int ypos, int width, int height) :
+    m_id {id}, m_name {name}, m_runespell {runespell}, m_xpos {xpos}, m_ypos {ypos}, m_width {width}, m_height { height} {}
+
+GlyphModel::GlyphModel() : GlyphModel(0, "", 0, 0, 0, 0, 0) {}
+
+int GlyphModel::id()
+{
+    return m_id;
+}
+
+QString GlyphModel::name()
+{
+    return m_name;
+}
+
+int GlyphModel::runespell()
+{
+    return m_runespell;
+}
+
+int GlyphModel::xpos()
+{
+    return m_xpos;
+}
+
+int GlyphModel::ypos()
+{
+    return m_ypos;
+}
+
+int GlyphModel::width()
+{
+    return m_width;
+}
+
+int GlyphModel::height()
+{
+    return m_height;
+}
+
+void GlyphModel::id(int id)
+{
+    m_id = id;
+}
+
+void GlyphModel::name(QString name)
+{
+    m_name = name;
+}
+
+void GlyphModel::runespell(int runespell)
+{
+    m_runespell = runespell;
+}
+
+void GlyphModel::xpos(int xpos)
+{
+    m_xpos = xpos;
+}
+
+void GlyphModel::ypos(int ypos)
+{
+    m_ypos = ypos;
+}
+
+void GlyphModel::width(int width)
+{
+    m_width = width;
+}
+
+void GlyphModel::height(int height)
+{
+    m_height = height;
+}
+
+bool GlyphModel::save()
+{
+    return false;
+}
+void GlyphModel::remove()
+{
+
+}
---
+++ runestones/glyphmodel.h
@@ -0,0 +1,45 @@
+#ifndef GLYPHMODEL_H
+#define GLYPHMODEL_H
+
+#include <QSqlQuery>
+#include <QString>
+
+class GlyphModel
+{
+public:
+    static QSqlQuery list(int runespell);
+    static GlyphModel load(int id);
+
+    GlyphModel(int id, QString name, int runespell, int xpos, int ypos, int width, int height);
+    GlyphModel();
+
+    int id();
+    QString name();
+    int runespell();
+    int xpos();
+    int ypos();
+    int width();
+    int height();
+
+    void id(int id);
+    void name(QString name);
+    void runespell(int runespell);
+    void xpos(int xpos);
+    void ypos(int ypos);
+    void width(int width);
+    void height(int height);
+
+    bool save();
+    void remove();
+
+private:
+    int m_id;
+    QString m_name;
+    int m_runespell;
+    int m_xpos;
+    int m_ypos;
+    int m_width;
+    int m_height;
+};
+
+#endif // GLYPHMODEL_H
